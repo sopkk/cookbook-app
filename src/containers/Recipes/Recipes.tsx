fd04cbbf8cfedd220927ef1e8117b2298870c663
sopkk <aleksandrasopka@yahoo.com>
2020-02-21 10:54:52 +0100

    Adding new recipes with name and image

--- src/containers/Recipes/Recipes.tsx
+++ src/containers/Recipes/Recipes.tsx
@@ -6,11 +6,17 @@ import RecipeDetails from "../../components/RecipeDetails/RecipeDetails";
 import getRecipes from "../../store/Recipes/selectors/getRecipes";
 import { IRecipe } from "../../store/Recipes/RecipeTypes";
 import Recipe from "../../components/Recipe/Recipe";
+import Button from "../../components/UI/Button/Button";
+import Modal, { ModalType } from "../../components/UI/Modal/Modal";
 import "./Recipes.css";
 
 const Recipes: React.FunctionComponent = () => {
   const [recipeShowed, setRecipeShowed] = React.useState<JSX.Element | string>(
     "Choose a recipe"
+  );
+
+  const [recipeModal, setRecipeModal] = React.useState<JSX.Element | null>(
+    null
   );
 
   const dispatch = useDispatch();
@@ -36,10 +42,45 @@ const Recipes: React.FunctionComponent = () => {
     );
   });
 
+  const closeModalHandler = () => {
+    setRecipeModal(null);
+  };
+
+  const recipeAddedHandler = (recipe: IRecipe) => {
+    console.log("recept pre dispatcha: ", recipe);
+    dispatch(recipesActions.addRecipe(recipe));
+    setRecipeModal(null);
+  };
+
+  const addNewRecipeHandler = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+    setRecipeModal(
+      <Modal
+        className="modal-add-new-recipe"
+        type={ModalType.Add}
+        onCloseHandler={closeModalHandler}
+        onRecipeAddedHandler={recipeAddedHandler}
+      />
+    );
+  };
+
   return (
-    <div style={{ display: "flex" }}>
-      <div> {recipes} </div>
-      <div style={{ display: "flex" }}>{recipeShowed}</div>
+    <div>
+      <Button
+        type="submit"
+        name="add-recipe"
+        className="add-recipe"
+        onClickHandler={addNewRecipeHandler}
+      >
+        Add new recipe
+      </Button>
+      {recipeModal}
+      <div style={{ display: "flex" }}>
+        <div> {recipes} </div>
+        <div style={{ display: "flex" }}>{recipeShowed}</div>
+      </div>
     </div>
   );
 };
--- src/store/Recipes/actions/actionTypes.ts
+++ src/store/Recipes/actions/actionTypes.ts
@@ -1,8 +1,25 @@
-import { IRecipeList } from "../RecipeTypes";
+import { IRecipeList, IRecipe } from "../RecipeTypes";
 
 export const GET_RECIPES_START = "GET_RECIPES_START";
 export const GET_RECIPES_SUCCESS = "GET_RECIPES_SUCCES";
 export const GET_RECIPES_FAILED = "GET_RECIPES_FAILED";
+
+export const ADD_RECIPE_START = "ADD_RECIPE_START";
+export const ADD_RECIPE_SUCCES = "ADD_RECIPE_SUCCES";
+export const ADD_RECIPE_FAILED = "ADD_RECIPE_FAILED";
+
+interface AddedRecipePayload {
+  id: string;
+  recipe: IRecipe;
+}
+
+export type RecipesActionTypes =
+  | GetRecipesStart
+  | GetRecipesSucces
+  | GetRecipesFailed
+  | AddRecipeStart
+  | AddRecipeSucces
+  | AddRecipeFailed;
 
 export interface GetRecipesStart {
   type: typeof GET_RECIPES_START;
@@ -17,7 +34,15 @@ export interface GetRecipesFailed {
   type: typeof GET_RECIPES_FAILED;
 }
 
-export type RecipesActionTypes =
-  | GetRecipesStart
-  | GetRecipesSucces
-  | GetRecipesFailed;
+export interface AddRecipeStart {
+  type: typeof ADD_RECIPE_START;
+}
+
+export interface AddRecipeSucces {
+  type: typeof ADD_RECIPE_SUCCES;
+  payload: AddedRecipePayload;
+}
+
+export interface AddRecipeFailed {
+  type: typeof ADD_RECIPE_FAILED;
+}
